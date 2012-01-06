7f897f194c3b88e928f009577cfc48767886cb2c
Takeru Ohta <phjgt308@gmail.com>
2012-01-06 09:52:11 +0900

    dic読み込み方法再修正

--- load-hashmap.lisp
+++ load-hashmap.lisp
@@ -1,11 +1,10 @@
-(eval-when (:load-toplevel :compile-toplevel :execute)
-  (defun root-path ()
-    (directory-namestring (or *compile-file-pathname* *load-pathname* #P"./")))
-  (defun load-local-system (package &optional (package-directory (root-path)))
+(eval-when (:compile-toplevel)
+  (defun load-local-system (package &optional (package-directory #P"./"))
     (let #.`((asdf:*central-registry* (directory package-directory))
              ;; or #+ASDF2
              ,@(when #.#1=(find-symbol "*DEFAULT-SOURCE-REGISTRIES*" :asdf)
                      `((,#1# nil))))
          (asdf:load-system package))))
 
-(load-local-system :dict-0.2.0 #P"lib/dict-0.2.0/")
+(load-local-system :dict-0.2.0 
+  #.(merge-pathnames #P"lib/dict-0.2.0/" *compile-file-pathname*))
